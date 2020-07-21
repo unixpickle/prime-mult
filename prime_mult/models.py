3516cc6088f8cb737e562866efc27e32543838ad
Alex Nichol <unixpickle@gmail.com>
2020-07-21 15:12:05 -0400

    add SIREN model

--- prime_mult/models.py
+++ prime_mult/models.py
@@ -1,9 +1,12 @@
+import torch
 import torch.nn as nn
 
 
 def named_model(name, num_bits):
     if name == "mlp":
         return MLPFactorizer(num_bits)
+    elif name == "siren":
+        return SIRENFactorizer(num_bits)
     raise ValueError(f"no such model: {name}")
 
 
@@ -22,3 +25,25 @@ class MLPFactorizer(nn.Module):
 
     def forward(self, x):
         return self.sequential(x)
+
+
+class SIRENFactorizer(nn.Module):
+    def __init__(self, num_bits, d=1024):
+        super().__init__()
+        self.sequential = nn.Sequential(
+            nn.Linear(num_bits * 2, d),
+            Sin(),
+            nn.Linear(d, d),
+            Sin(),
+            nn.Linear(d, d),
+            Sin(),
+            nn.Linear(d, num_bits * 2),
+        )
+
+    def forward(self, x):
+        return self.sequential(x)
+
+
+class Sin(nn.Module):
+    def forward(self, x):
+        return torch.sin(x)
