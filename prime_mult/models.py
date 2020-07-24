99c6ee59f7c58bd27e16de09367576dce7e5d249
Alex Nichol <unixpickle@gmail.com>
2020-07-24 16:19:02 -0400

    add hardcoded baseline to make sure everything is setup right

--- prime_mult/models.py
+++ prime_mult/models.py
@@ -7,6 +7,8 @@ def named_model(name, num_bits):
         return MLPFactorizer(num_bits)
     elif name == "siren":
         return SIRENFactorizer(num_bits)
+    elif name == "hardcoded":
+        return HardCodedFactorizer(num_bits)
     raise ValueError(f"no such model: {name}")
 
 
@@ -47,3 +49,35 @@ class SIRENFactorizer(nn.Module):
 class Sin(nn.Module):
     def forward(self, x):
         return torch.sin(x)
+
+
+class HardCodedFactorizer(nn.Module):
+    def __init__(self, num_bits):
+        super().__init__()
+        self.num_bits = num_bits
+        self.logit_scale = nn.Parameter(torch.tensor([1.0]))
+
+    def forward(self, x):
+        results = []
+        for elem in x:
+            results.append(self._multiply(elem))
+        return torch.stack(results) * self.logit_scale
+
+    def _multiply(self, pq_pair):
+        values = (pq_pair > 0.5).detach().cpu().numpy().tolist()
+        p = _bits_to_number(values[: self.num_bits])
+        q = _bits_to_number(values[self.num_bits :])
+        pq = p * q
+        return _number_to_tensor(self.num_bits * 2, pq).to(pq_pair)
+
+
+def _bits_to_number(bits):
+    result = 0
+    for i, b in enumerate(bits):
+        if b:
+            result |= 1 << i
+    return result
+
+
+def _number_to_tensor(num_bits, n):
+    return torch.tensor([(1.0 if (n & (1 << i)) else -1.0) for i in range(num_bits)])
